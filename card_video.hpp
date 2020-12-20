0f7c0a725e4a8d93f4c1dcf736679215388e289f
aliensowo <natsume.ace@gmail.com>
2020-12-20 16:17:50 +0300

    complete child hpp class

--- card_video.hpp
+++ card_video.hpp
@@ -1,11 +1,53 @@
 #ifndef CARD_VIDEO_HPP
 #define CARD_VIDEO_HPP
 
+#include "card_base.hpp"
 
-class card_video
+class CardVideo: public BaseDetail
 {
 public:
-    card_video();
+    enum TVideoCardType {undefine=0, discrete, build_in, hybrid, external};
+
+private:
+    TVideoCardType videocardtype;
+    std::string speed;
+    int hdmi_input;
+    std::string max_resolution;
+    std::string amount_of_video_memory;
+
+public:
+    CardVideo();
+    CardVideo(const Title l_title,
+              const TVideoCardType l_videocardtype,
+              const std::string lspeed,
+              const int l_hdmi_input,
+              const std::string l_max_resolutoin,
+              const std::string l_amount_of_video_memory,
+              const std::string l_year_of_manufacture,
+              const bool l_guarantee);
+
+    CardVideo ( const CardVideo &other);
+
+    ~CardVideo();
+
+    CardVideo &operator=(const CardVideo &other);
+
+    void setVideoCardType(const TVideoCardType l_videocardtype);
+    TVideoCardType getVideocardType() const;
+
+    void setCardSpeed(const std::string l_speed);
+    std::string getCardSpeed() const;
+
+    void setHDMI_input(const int l_hdmi_input);
+    int getHDMI_input() const;
+
+    void setMaxResolution(const std::string l_max_resolution);
+    std::string getMaxResolution() const;
+
+    void setAmountOfVideoMemory(const std::string l_amount_of_video_memory);
+    std::string getAmountOfVideoMemory() const;
+
+    void show();
 };
 
 #endif // CARD_VIDEO_HPP
